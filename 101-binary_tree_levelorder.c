1a4c7f386c53778c6ea1e7a1c28051d5eaa9a036
etojama <torohkiss@gmail.com>
2022-08-26 16:44:40 -0700

    task edit

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -42,11 +42,10 @@ void goes_in_level(const binary_tree_t *tree, void (*func)(int), size_t level)
 
 /**
  * binary_tree_levelorder - goes through a binary tree using
- * level-order traversal
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node.
- * @func: a pointer to a function to call for each node.
  */
+
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
